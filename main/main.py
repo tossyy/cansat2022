76ddc1fb9dcad2fc242f93a103d08779ee3ee959
坂本圭士郎 <k.sakamoto@sakamotokeishirounoMacBook-Air.local>
2022-05-14 22:26:17 +0900

    [Modify] main

--- main/main.py
+++ main/main.py
@@ -1,15 +1,15 @@
-import pigpio as pg
+import pigpio as pig
 import take_pic
 import time
 
 #ピンの設定
 
-pi = pg.pi()
+pi = pig.pi()
 
 moter_r = 18
 moter_l = 19
 
-pi.set_mode(moter_r, pg.OUTPUT)
-pi.set_mode(moter_l, pg.OUTPUT)
+pi.set_mode(moter_r, pig.OUTPUT)
+pi.set_mode(moter_l, pig.OUTPUT)
 
 
