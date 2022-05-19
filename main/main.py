8d8dea76aa59e66d420b330e93aad1a74c2be90c
坂本圭士郎 <k.sakamoto@sakamotokeishirounoMacBook-Air.local>
2022-05-19 10:20:24 +0900

    [Modify] main.py

--- main/main.py
+++ main/main.py
@@ -1,15 +1,6 @@
-import pigpio as pig
-import take_pic
-import time
+from machine import Machine
 
-#ピンの設定
-
-pi = pig.pi()
-
-moter_r = 18
-moter_l = 19
-
-pi.set_mode(moter_r, pig.OUTPUT)
-pi.set_mode(moter_l, pig.OUTPUT)
-
-
+ma = Machine()
+print( "hajimemasu")
+ma.run()
+print("owarimasita")
