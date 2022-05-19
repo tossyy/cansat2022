e004dda413f2296a84598537903257061b84c6d1
坂本圭士郎 <k.sakamoto@sakamotokeishirounoMacBook-Air.local>
2022-05-19 10:21:38 +0900

    [Modify] machine.py

--- functions/machine.py
+++ functions/machine.py
@@ -1,32 +1,43 @@
-#動きの関数を入れます
 import pigpio as pig
 
-moter_r1 = 1
-moter_l1 = 2
-moter_r2 = 3
-moter_l2 = 4
+motor_r1 = 1
+motor_l1 = 2
+motor_r2 = 3
+motor_l2 = 4
+SENSA_9 = 5
 
-class Moter:
+class Motor:
 
     def __init__(self, pi):
         self.pi = pi
-        pi.set_mode(moter_r1, pig.OUTPUT)
-        pi.set_mode(moter_l1, pig.OUTPUT)
+        pi.set_mode(motor_r1, pig.OUTPUT)
+        pi.set_mode(motor_l1, pig.OUTPUT)
 
-    def moter_r_forward(self):
-        self.pi.set()
+    def motor_r_forward(self):
+        self.pi.write(motor_)
+
+class Sensa_9:
+
+    def __init__(self, pi):
+        self.pi = pi
+
+
 
 class Machine:
 
     def __init__(self):
         self.pi = pig.pi()
-        self.moter = Moter(self.pi)
+        self.motor = Motor(self.pi)
         self.sensa_9 = Sensa_9(self.pi)
         self.sensa_ato = Sensa_ato(self.pi)
 
-    def phase1():
+    def phase1(self):
+
+
+    def phase2(self):
+
 
     def run():
         phase1()
         print(owari)
-        pahse2()
+        phase2()
