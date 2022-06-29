4f2a183ad522788ef5769b97b2dcad79c0fa4b98
tossyy <t.katsube1014@gmail.com>
2022-06-30 01:00:42 +0900

    モータークラスを追加

--- main/motor.py
+++ main/motor.py
@@ -3,138 +3,92 @@
 #GPIOライブラリをインポート
 import RPi.GPIO as GPIO
 
-#timeライブラリをインポート
-import time
+class Motor:
 
-#ピン番号の割り当て方式を「コネクタのピン番号」に設定
-GPIO.setmode(GPIO.BCM)
+    def __init__(self):
 
-#使用するピン番号を代入
-AIN1 = 10
-AIN2 = 9
-PWMA = 11
+        #ピン番号の割り当て方式を「コネクタのピン番号」に設定
+        GPIO.setmode(GPIO.BCM)
 
-BIN1 = 22
-BIN2 = 27
-PWMB = 17
+        #使用するピン番号
+        self.AIN1 = 10
+        self.AIN2 = 9
+        self.PWMA = 11
 
-#各ピンを出力ピンに設定
-GPIO.setup(AIN1, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(AIN2, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(PWMA, GPIO.OUT, initial = GPIO.LOW)
+        self.BIN1 = 22
+        self.BIN2 = 27
+        self.PWMB = 17
 
-GPIO.setup(BIN1, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(BIN2, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(PWMB, GPIO.OUT, initial = GPIO.LOW)
+        #各ピンを出力ピンに設定
+        GPIO.setup(AIN1, GPIO.OUT, initial = GPIO.LOW)
+        GPIO.setup(AIN2, GPIO.OUT, initial = GPIO.LOW)
+        GPIO.setup(PWMA, GPIO.OUT, initial = GPIO.LOW)
 
-#PWMオブジェクトのインスタンスを作成
-#出力ピン：12,26  周波数：100Hz
-p_a = GPIO.PWM(PWMA,100)
-p_b = GPIO.PWM(PWMB,100)
+        GPIO.setup(BIN1, GPIO.OUT, initial = GPIO.LOW)
+        GPIO.setup(BIN2, GPIO.OUT, initial = GPIO.LOW)
+        GPIO.setup(PWMB, GPIO.OUT, initial = GPIO.LOW)
 
-#PWM信号を出力
-p_a.start(0)
-p_b.start(0)
+        #PWMオブジェクトのインスタンスを作成
+        #出力ピン：12,26  周波数：100Hz
+        self.p_a = GPIO.PWM(PWMA,100)
+        self.p_b = GPIO.PWM(PWMB,100)
 
-#デューティを設定(0~100の範囲で指定)
-#速度は80%で走行する。
-val = 80
+        #PWM信号を出力
+        self.p_a.start(0)
+        self.p_b.start(0)
 
-#デューティ比を設定
-p_a.ChangeDutyCycle(val)
-p_b.ChangeDutyCycle(val)
+        #デューティを設定(0~100の範囲で指定)
+        #速度は80%で走行する。
+        val = 80
 
-#ブレーキする関数
-def func_brake():
-    GPIO.output(AIN1, GPIO.HIGH)
-    GPIO.output(AIN2, GPIO.HIGH)
-    GPIO.output(BIN1, GPIO.HIGH)
-    GPIO.output(BIN2, GPIO.HIGH)
+        #デューティ比を設定
+        self.p_a.ChangeDutyCycle(val)
+        self.p_b.ChangeDutyCycle(val)
 
-
-#前進する関数
-def func_forward():
-    GPIO.output(AIN1, GPIO.LOW)
-    GPIO.output(AIN2, GPIO.HIGH)
-    GPIO.output(BIN1, GPIO.LOW)
-    GPIO.output(BIN2, GPIO.HIGH)
+    #ブレーキする関数
+    def func_brake(self):
+        GPIO.output(self.AIN1, GPIO.HIGH)
+        GPIO.output(self.AIN2, GPIO.HIGH)
+        GPIO.output(self.BIN1, GPIO.HIGH)
+        GPIO.output(self.BIN2, GPIO.HIGH)
 
 
-#後進する関数
-def func_back():
-    GPIO.output(AIN1, GPIO.HIGH)
-    GPIO.output(AIN2, GPIO.LOW)
-    GPIO.output(BIN1, GPIO.HIGH)
-    GPIO.output(BIN2, GPIO.LOW)
+    #前進する関数
+    def func_forward(self):
+        GPIO.output(self.AIN1, GPIO.LOW)
+        GPIO.output(self.AIN2, GPIO.HIGH)
+        GPIO.output(self.BIN1, GPIO.LOW)
+        GPIO.output(self.BIN2, GPIO.HIGH)
 
 
-#右回転する関数
-def func_right():
-    GPIO.output(AIN1, GPIO.LOW)
-    GPIO.output(AIN2, GPIO.HIGH)
-    GPIO.output(BIN1, GPIO.HIGH)
-    GPIO.output(BIN2, GPIO.LOW)
+    #後進する関数
+    def func_back(self):
+        GPIO.output(self.AIN1, GPIO.HIGH)
+        GPIO.output(self.AIN2, GPIO.LOW)
+        GPIO.output(self.BIN1, GPIO.HIGH)
+        GPIO.output(self.BIN2, GPIO.LOW)
 
 
-#左回転する関数
-def func_left():
-    GPIO.output(AIN1, GPIO.HIGH)
-    GPIO.output(AIN2, GPIO.LOW)
-    GPIO.output(BIN1, GPIO.LOW)
-    GPIO.output(BIN2, GPIO.HIGH)
+    #右回転する関数
+    def func_right(self):
+        GPIO.output(self.AIN1, GPIO.LOW)
+        GPIO.output(self.AIN2, GPIO.HIGH)
+        GPIO.output(self.BIN1, GPIO.HIGH)
+        GPIO.output(self.BIN2, GPIO.LOW)
 
 
-#メインプログラム
-while True:
-        
-    #3秒前進する
-    func_forward()
-    time.sleep(3.0)        
-    #3秒ブレーキ
-    func_brake()
-    time.sleep(3.0)
+    #左回転する関数
+    def func_left(self):
+        GPIO.output(self.AIN1, GPIO.HIGH)
+        GPIO.output(self.AIN2, GPIO.LOW)
+        GPIO.output(self.BIN1, GPIO.LOW)
+        GPIO.output(self.BIN2, GPIO.HIGH)
+
+    def close(self):
+        #PWM信号を停止
+        self.p_a.stop()
+        self.p_b.stop()
+
+        #GPIOを開放
+        GPIO.cleanup()
     
-    
-    #1.5秒右回転する
-    func_right()
-    time.sleep(1.5)
-    #3秒ブレーキ
-    func_brake()
-    time.sleep(3.0)
-    
-
-    #2.8秒左回転する
-    func_left()
-    time.sleep(2.8)
-    #3秒ブレーキ
-    func_brake()
-    time.sleep(3.0)
-
-
-    #1.5秒右回転する
-    func_right()
-    time.sleep(1.5)
-    #3秒ブレーキ
-    func_brake()
-    time.sleep(3.0) 
-
-
-    #3秒後進する
-    func_back()
-    time.sleep(3.0)
-    #3秒ブレーキ
-    func_brake()
-    time.sleep(3.0)
-    
-    break
-    
-
-#PWM信号を停止
-p_a.stop()
-p_b.stop()
-
-#GPIOを開放
-GPIO.cleanup()
-
-#プログラム終了
-print("End of program")
