12feeb25b84f5ff3d12392c324a443d0dbe65199
tossyy <t.katsube1014@gmail.com>
2022-07-06 16:37:29 +0900

    南北判定追加

--- main/calibrate.py
+++ main/calibrate.py
@@ -3,3 +3,13 @@ from machine import Machine
 ma = Machine()
 ma.motor.change_speed(30)
 ma.nine.calibrate(ma.motor)
+
+while True:
+    c = input()
+    if c == "q":
+        break
+    if c == "a":
+        mag_value = ma.nine.get_mag_value_corrected()
+        print(mag_value)
+
+ma.close()
