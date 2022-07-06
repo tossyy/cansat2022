987cfff2bc64bd5b8cabf30ecd8001245410a39a
laika90 <momoshinak46@gmail.com>
2022-07-06 15:51:29 +0900

    9軸csv

---
+++ unit_test/9-axis-sensor/9_anim.py
@@ -0,0 +1,32 @@
+import sys
+import csv
+
+sys.path.append("/home/pi/utat/cansat2022/main")
+
+from machine import Machine
+
+ma = Machine()
+
+t_start = time.prefcounter()
+ma.motor.speed_change(20)
+ma.motor.func_right()
+
+xs = []
+ys = []
+
+while time.prefcounter() - t_start < 20 :
+    xs.append(ma.nine.get_mag_value()[0])
+    ys.append(ma.nine.get_mag_value()[1])
+    time.sleep(0.1)
+
+with open("/home/pi/utat/calib_data.csv", "w") as f:
+    csv_obj = csv.witter(f)
+    csv_obj.writerow(xs)
+    csv_obj.writerow(ys)
+
+
+
+
+
+
+
