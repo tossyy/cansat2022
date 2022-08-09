950405b3c050b02a436cf5f9d1adf4fa25cc4d90
laika90 <momoshinak46@gmail.com>
2022-08-09 14:36:10 +0900

    ayuusei

--- main/main.py
+++ main/main.py
@@ -1,14 +1,10 @@
 from machine import Machine
 
-try:
-    print("Machine setting up...")
-    ma = Machine()
-    print("Set up finished")
 
-    print("Mision start")
-    ma.run()
-    print("Mission complete")
-except Exception as e:
-    print(e)
-finally:
-    ma.close()
+print("Machine setting up...")
+ma = Machine()
+print("Set up finished")
+
+print("Mision start")
+ma.run()
+print("Mission complete")
