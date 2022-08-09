f3f6686d158c7d1a094cb6f0dc605ace578a02bb
laika90 <momoshinak46@gmail.com>
2022-08-09 15:00:58 +0900

    ma

--- main/machine.py
+++ main/machine.py
@@ -575,8 +575,8 @@ if __name__ == "__main__":
     ma = Machine()
     try:
         time.sleep(5)
-        ma.phase5()
-        #ma.phase6()
+        #ma.phase5()
+        ma.phase6()
         #ma.phase7()
 
     except Exception as e:
