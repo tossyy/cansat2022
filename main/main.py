81d56f55e955b7193d0dc7e021eac670b1e3e336
laika90 <momoshinak46@gmail.com>
2022-08-09 12:43:34 +0900

    暗闇判定を追加しました

--- main/main.py
+++ main/main.py
@@ -1,9 +1,14 @@
 from machine import Machine
 
-print("Machine setting up...")
-ma = Machine()
-print("Set up finished")
+try:
+    print("Machine setting up...")
+    ma = Machine()
+    print("Set up finished")
 
-print("Mision start")
-ma.run()
-print("Mission complete")
+    print("Mision start")
+    ma.run()
+    print("Mission complete")
+except Exception as e:
+    print(e)
+finally:
+    ma.close()
