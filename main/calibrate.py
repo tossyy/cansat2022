d40cf88141ea41123f13015049455842e79dccde
laika90 <momoshinak46@gmail.com>
2022-08-13 15:53:00 +0900

    一周

--- main/calibrate.py
+++ main/calibrate.py
@@ -1,15 +1,20 @@
 from machine import Machine
+import time
 
 ma = Machine()
-ma.motor.change_speed(30)
-ma.calibrate()
+# ma.motor.change_speed(25)
+# ma.calibrate()
 
-while True:
-    c = input()
-    if c == "q":
-        break
-    if c == "a":
-        mag_value = ma.nine.get_mag_value_corrected()
-        print(mag_value)
+# while True:
+#     c = input()
+#     if c == "q":
+#         break
+#     if c == "a":
+#         mag_value = ma.nine.get_mag_value_corrected()
+#         print(mag_value)
+
+ma.motor.func_right()
+time.sleep(30)
+ma.motor.func_brake()
 
 ma.close()
