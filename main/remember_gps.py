38a71442d671205f3d1f2ed4c35f5bc735a0b9d9
laika90 <momoshinak46@gmail.com>
2022-08-13 12:42:54 +0900

    gps値が取れない時の

--- main/remember_gps.py
+++ main/remember_gps.py
@@ -4,9 +4,7 @@ print("Machine setting up...")
 ma = Machine()
 print("Set up finished")
 
-try:
-    ma.remember_gps()
-except UnicodeDecodeError:
-    ma.remember_gps()
-    
+
+ma.remember_gps()
+
 ma.close()
